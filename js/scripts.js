41de73ecce6d2d1afee1bc38502e56c53fe26cb6
amryousry30 <amryousry30@gmail.com>
2025-07-26 01:18:09 +0300

    add Certificates section and update styles

--- js/scripts.js
+++ js/scripts.js
@@ -679,6 +679,19 @@ if ($('.image-link').length) {
       },
       
 
-    });// JavaScript Document
+    }); // JavaScript Document
 }
+
+  // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
 } )( jQuery );
