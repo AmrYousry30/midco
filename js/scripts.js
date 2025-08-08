b247f29b499540804c38b687e2f37148cbf1b82c
amryousry30 <amryousry30@gmail.com>
2025-08-09 00:38:47 +0300

    update nav styles

--- js/scripts.js
+++ js/scripts.js
@@ -689,15 +689,15 @@ if ($('.image-link').length) {
     $(window).scroll(function () {
         if ($(window).width() < 992) {
             if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-dark shadow');
+                $('.fixed-top').addClass('bg-gray shadow');
             } else {
-                $('.fixed-top').removeClass('bg-dark shadow');
+                $('.fixed-top').removeClass('bg-gray shadow');
             }
         } else {
             if ($(this).scrollTop() > 45) {
-                $('.fixed-top').addClass('bg-dark shadow').css('top', -45);
+                $('.fixed-top').addClass('bg-gray shadow').css('top', -45);
             } else {
-                $('.fixed-top').removeClass('bg-dark shadow').css('top', 0);
+                $('.fixed-top').removeClass('bg-gray shadow').css('top', 0);
             }
         }
     });
